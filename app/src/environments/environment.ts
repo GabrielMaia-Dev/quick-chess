92cc83d654cf2c6e31e23d86d6de974fafdaa39c
Gabriel Maia <gab.maia@outlook.com>
2023-08-14 22:59:29 -0400

    dominio https

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://quickchess.azurewebsites.net',
-    chessHubUrl: 'http://quickchess.azurewebsites.net/hub'
+    apiUrl: 'https://quickchess.azurewebsites.net',
+    chessHubUrl: 'https://quickchess.azurewebsites.net/hub'
 };
